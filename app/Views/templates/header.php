169127f7b0bbe68ef2a021914f85566641ec2c39
Leonardo Marcatti da Silva <leonardomarcatti@hotmail.com>
2025-05-25 20:37:40 -0300

    CSS improvements

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -34,17 +34,46 @@ $pager = \Config\Services::pager();
                Planti
             </a>
             <ul class="dropdown-menu">
-               <li><a class="dropdown-item" href="<?= base_url('tipos') ?>">Cadastro de Tipos</a></li>
-               <li><a class="dropdown-item" href="<?= base_url('cadastroPlanta') ?>">Cadastro de Plantas</a></li>
+               <li>
+                  <a class="dropdown-item" href="<?= base_url('tipos') ?>">
+                     <i class="fa-solid fa-ellipsis-vertical"></i> Cadastro de Tipos
+                  </a>
+               </li>
+               <li>
+                  <a class="dropdown-item" href="<?= base_url('cadastroPlanta') ?>">
+                     <i class="fa-solid fa-leaf"></i> Cadastro de Plantas
+                  </a>
+               </li>
                <li>
                   <hr class="dropdown-divider">
                </li>
-               <li><a class="dropdown-item" href="<?= base_url('cuidadosTipos') ?>">Cuidados - Por Tipo</a></li>
-               <li><a class="dropdown-item" href="<?= base_url('cuidadosTodas') ?>">Cuidados - Todas</a></li>
+               <li>
+                  <a class="dropdown-item" href="<?= base_url('cuidadosTipos') ?>">
+                     <i class="fa-solid fa-check-double"></i> Cuidados - Por Tipo
+                  </a>
+               </li>
+               <li>
+                  <a class="dropdown-item" href="<?= base_url('cuidadosTodas') ?>">
+                     <i class="fa-solid fa-check-double"></i> Cuidados - Todas
+                  </a>
+               </li>
                <li>
                   <hr class="dropdown-divider">
                </li>
-               <li><a class="dropdown-item" href="<?= base_url('logout') ?>">Logout</a></li>
+               <li>
+                  <a href="" class="dropdown-item editUser">
+                     <i class="fa-solid fa-user-pen"></i> Editar Usuário/Senha
+                  </a>
+               </li>
+               <li>
+                  <hr class="dropdown-divider">
+               </li>
+               <li>
+                  <a class="dropdown-item logout" href="<?= base_url('logout') ?>">
+                     <i class="fa-solid fa-power-off"></i> Logout
+                  </a>
+               </li>
+
             </ul>
          </div>
       </div>
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,9 +1,11 @@
 const btns = document.querySelectorAll('.btnDelCuidado');
 const delLink = document.querySelector('#delLink').getAttribute('href');
 
-btns.forEach((el) => {
- el.addEventListener('click', function (e) {
-  const value = e.target.value;
-  document.querySelector('#delLink').setAttribute('href', delLink+value);
- });
-});
+if (btns && delLink) {
+   btns.forEach((el) => {
+      el.addEventListener('click', function (e) {
+         const value = e.target.value;
+         document.querySelector('#delLink').setAttribute('href', delLink + value);
+      });
+   });
+}
